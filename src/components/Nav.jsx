2b2d3ca1b403d40790653433241523cb5855a1a4
Omar Taouil <63510451+OmarTl2005@users.noreply.github.com>
2023-11-01 00:07:52 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export const Nav = () => {
     return (
         <header className="h-[191px] xsm:h-[170px] m-0 p-0 w-screen flex items-center justify-around flex-row top-0">
             <div className="self-start h-full w-[200px] m-0 p-0 xsm:h-[5%] xsm:hidden sups:hidden xsm:flex sm:flex lg:flex xl:flex">
-                <a href="/"><img className="xsm:w-full cursor-pointer hover:-translate-y-2 transition-all duration-700" alt="Voxify Logo" src={ logo } style={{ margin: 0, padding: 0, height: '180px', width: '200px' }} /></a>
+                <a href="https://omartl2005.github.io/Voxify"><img className="xsm:w-full cursor-pointer hover:-translate-y-2 transition-all duration-700" alt="Voxify Logo" src={ logo } style={{ margin: 0, padding: 0, height: '180px', width: '200px' }} /></a>
             </div>
             <div className="w-[15%] h-full self-end flex items-center justify-around flex-row xsm:w-full xsm:h-full sups:w-full xl:w-[20%] md:w-[20%]">
                 <a href="https://www.instagram.com/its_omar_00/" target="_blank" rel="noreferrer" ><PiInstagramLogoDuotone className="hover:text-pink-700 text-purple-500 transition-all duration-[600ms] hover:-translate-y-1 relative text-5xl xsm:text-3xl xl:text-5xl" /></a>
